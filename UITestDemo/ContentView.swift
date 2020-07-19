5e7a6daae1148f63a42537e1dc37b5863283dc5f
Kyl <chiangkaiyun@gmail.com>
2020-07-19 15:01:06 +0800

    Initial Commit

---
+++ UITestDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITestDemo
+//
+//  Created by N2120008436 on 2020/7/19.
+//  Copyright © 2020 KylChiang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
