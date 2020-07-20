7e230b60e98d964d4aa08cf4c4386c69374a76fd
Kyl <chiangkaiyun@gmail.com>
2020-07-20 11:57:06 +0800

    Add access token check & member info request

--- UITestDemo/ContentView.swift
+++ UITestDemo/ContentView.swift
@@ -7,15 +7,42 @@
 //
 
 import SwiftUI
+import FBSDKLoginKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Button(action: {
+            if let token = AccessToken.current, !token.isExpired {
+                self.getMemberInfo()
+            } else {
+                self.loginFB()
+            }
+        }) {
+            Text("Facebook Login")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    func loginFB() {
+        let loginManager = LoginManager()
+        loginManager.logIn(permissions: ["public_profile", "email"], from: nil) { (result, error) in
+            self.getMemberInfo()
+        }
+    }
+    
+    func getMemberInfo() {
+        let request = GraphRequest(graphPath: "me", parameters: ["fields": "email, name, picture.type(normal)"])
+        request.start { (_, result, error) in
+            if let data = result as? [String: Any] {
+                print("result: \(data)")
+            }
+        }
     }
 }
