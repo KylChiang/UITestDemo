bf94b42390340f2cdf7398f2f6a551e7ec9e57d8
Kyl <chiangkaiyun@gmail.com>
2020-07-21 12:16:51 +0800

    Add testFBLogin()

--- UITestDemoUITests/UITestDemoUITests.swift
+++ UITestDemoUITests/UITestDemoUITests.swift
@@ -9,35 +9,41 @@
 import XCTest
 
 class UITestDemoUITests: XCTestCase {
+    let app = XCUIApplication()
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+    func testFBLogin() throws {
+        // 1. tap login
+        app.buttons["Facebook Login"].tap()
+        // 2. tap alert
+        let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
+        let allowBtn = springboard.buttons["Continue"]
+        if allowBtn.waitForExistence(timeout: 10) {
+            allowBtn.tap()
         }
+        
+        // 3. tap FB [繼續] to login
+        let loginPage = app.webViews
+        loginPage.buttons["繼續"].tap()
+        
+        sleep(5)    // wait for the web login page loading finish
+        
+        // 4. back to first page, and check ID label is not empty with title "ID:"
+        let predicate = NSPredicate(format: "label CONTAINS %@", "ID:")
+        let label = app.staticTexts.matching(predicate).firstMatch
+        XCTAssert(label.label.count > 4)
+        
+        // 5. logout, idle
+        app.buttons["Logout"].tap()
     }
 }
